485779c62347bd6cf7a0363883d7f34304749b06
woozie <gabrielgb.akcr@gmail.com>
2025-03-21 01:09:30 -0300

    modified:   panificadora_db.sql

--- panificadora_db.sql
+++ panificadora_db.sql
@@ -83,7 +83,7 @@ CREATE TABLE `logs` (
   PRIMARY KEY (`id`),
   KEY `fk_usuario` (`usuario_id`),
   CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +92,7 @@ CREATE TABLE `logs` (
 
 LOCK TABLES `logs` WRITE;
 /*!40000 ALTER TABLE `logs` DISABLE KEYS */;
-INSERT INTO `logs` VALUES (1,1,'Excluiu usuÃ¡rio \'funcionario1\' (vendedor)','2025-03-20 02:12:22'),(2,1,'Adicionou usuÃ¡rio \'eduardo \' (gerente)','2025-03-20 02:13:30'),(3,1,'Adicionou usuÃ¡rio \'marcus\' (vendedor)','2025-03-20 02:34:37');
+INSERT INTO `logs` VALUES (1,1,'Excluiu usuÃ¡rio \'funcionario1\' (vendedor)','2025-03-20 02:12:22'),(2,1,'Adicionou usuÃ¡rio \'eduardo \' (gerente)','2025-03-20 02:13:30'),(3,1,'Adicionou usuÃ¡rio \'marcus\' (vendedor)','2025-03-20 02:34:37'),(4,1,'Registrou venda de 100 unidade(s) de \'PÃ£o de trigo\'','2025-03-21 02:25:09'),(5,1,'Excluiu produto \'PÃ£o de trigo\'','2025-03-21 02:44:24'),(6,1,'Adicionou produto \'PÃ£o de trigo\'','2025-03-21 02:45:00'),(7,1,'Adicionou produto \'pÃ£o de queijo\'','2025-03-21 02:45:27'),(8,1,'Adicionou produto \'enroladinho de salsicha\'','2025-03-21 02:46:00'),(9,1,'Adicionou produto \'sonho\'','2025-03-21 02:46:40'),(10,1,'Registrou venda de 19 unidade(s) de \'PÃ£o de trigo\'','2025-03-21 02:47:01'),(11,1,'Registrou venda de 25 unidade(s) de \'pÃ£o de queijo\'','2025-03-21 02:47:01'),(12,1,'Registrou venda de 11 unidade(s) de \'enroladinho de salsicha\'','2025-03-21 02:47:01'),(13,1,'Registrou venda de 6 unidade(s) de \'sonho\'','2025-03-21 02:47:01');
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +116,7 @@ CREATE TABLE `produtos` (
   PRIMARY KEY (`id`),
   KEY `fk_categoria` (`categoria_id`),
   CONSTRAINT `fk_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +125,7 @@ CREATE TABLE `produtos` (
 
 LOCK TABLES `produtos` WRITE;
 /*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (9,'PÃ£o de trigo','',1000,11.00,NULL,16,8,'2025-03-22');
+INSERT INTO `produtos` VALUES (10,'PÃ£o de trigo','',81,0.30,NULL,20,8,NULL),(11,'pÃ£o de queijo','',175,0.35,NULL,40,8,NULL),(12,'enroladinho de salsicha','',39,2.50,NULL,6,10,NULL),(13,'sonho','',24,2.00,NULL,10,9,NULL);
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -200,7 +200,7 @@ CREATE TABLE `vendas` (
   PRIMARY KEY (`id`),
   KEY `idx_produto_id` (`produto_id`),
   CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,6 +209,7 @@ CREATE TABLE `vendas` (
 
 LOCK TABLES `vendas` WRITE;
 /*!40000 ALTER TABLE `vendas` DISABLE KEYS */;
+INSERT INTO `vendas` VALUES (6,10,19,'2025-03-21 02:47:01',0.30),(7,11,25,'2025-03-21 02:47:01',0.35),(8,12,11,'2025-03-21 02:47:01',2.50),(9,13,6,'2025-03-21 02:47:01',2.00);
 /*!40000 ALTER TABLE `vendas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -221,4 +222,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-20 22:46:13
+-- Dump completed on 2025-03-21  1:04:10
